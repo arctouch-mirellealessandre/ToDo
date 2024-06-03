46c169e896e599fbccae1be86fa363e788b069fb
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-06-03 09:54:56 -0300

    Create task model

---
+++ TaskUnity.swift
@@ -0,0 +1,15 @@
+//
+//  TaskUnity.swift
+//  ToDo
+//
+//  Created by Mirelle Alessandre on 03/06/24.
+//
+
+import Foundation
+
+struct TaskUnity {
+	let dueDate: String
+	let description: String
+	let id: String
+	let completed: Bool
+}
