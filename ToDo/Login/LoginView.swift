2d6c1540953a7564450cbc299c95df3a443c6650
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-06-20 10:00:49 -0300

    Refactor LoginView
    From now on, write all the coding regarding one Screen in one file for learning purposes. At the end, organize each one in its appropriate file/group.

--- ToDo/Login/LoginView.swift
+++ ToDo/Login/LoginView.swift
@@ -7,22 +7,66 @@
 
 import SwiftUI
 
+struct User: Codable {
+	var firstName: String
+	var email: String
+	var username: String
+	var id: String
+	var lastName: String
+	var token: String
+}
+
 final class UserManager: ObservableObject {
 	@Published var userState: UserState = .none
+	@Published var user: User?
+}
+
+final class LoginViewModel: ObservableObject {
+	private var userManager: UserManager
+	private var user: User?
+	
+	init(userManager: UserManager) {
+		self.userManager = userManager
+		self.user = nil
+	}
+	
+	func postLoginRequest(username: String, password: String) async throws -> User? {
+		guard let url = URL(string: "http://0.0.0.0:8080/v1/users/login") else {
+			throw LoginRequest.invalidURL
+		}
+		
+		var request = URLRequest(url: url)
+		request.httpMethod = "POST"
+		
+		let basicAuth = (username + ":" + password).data(using: .utf8)!.base64EncodedString()
+		request.addValue("Basic \(basicAuth)", forHTTPHeaderField: "Authorization")
+		
+		let (data, response) = try await URLSession.shared.data(for: request)
+		
+		guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+			throw LoginRequest.invalidResponse
+		}
+	
+		let user = try JSONDecoder().decode(User.self, from: data)
+		userManager.user = user
+		userManager.userState = .authorized
+		return user
+	}
 }
 
 struct LoginView: View {
 	@State private var username: String
 	@State private var password: String
-	private var userManager: UserManager
 	
-	init(userManager: UserManager) {
-		self.username = ""
-		self.password = ""
-		self.userManager = userManager
+	@ObservedObject private var loginViewModel: LoginViewModel
+	
+	init(loginViewModel: LoginViewModel) {
+		self.username = "john.doe"
+		self.password = "123456789"
+		self.loginViewModel = loginViewModel
 	}
 	
-    var body: some View {
+	var body: some View {
 		VStack {
 			Text("Login")
 				.bold()
@@ -35,16 +79,18 @@ struct LoginView: View {
 				}
 			Button {
 				Task {
-					let user = try await PostLogin().postLoginRequest(username: username, password: password)
-					if user != nil {
-						userManager.userState = .authorized
-					}
+					_ = try await loginViewModel.postLoginRequest(username: username, password: password)
 				}
 			} label: {
 				Text("Login")
 			}
 		}
-    }
+	}
+}
+
+enum LoginRequest: Error {
+	case invalidURL
+	case invalidResponse
 }
 
 enum UserState {
@@ -53,5 +99,5 @@ enum UserState {
 }
 
 #Preview {
-	LoginView(userManager: UserManager())
+	LoginView(loginViewModel: LoginViewModel(userManager: UserManager()))
 }
