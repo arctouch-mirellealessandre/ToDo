8dee350ed4c9d6a15d2b0adc346e062d482ee892
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-07-28 09:42:59 -0300

    Refactor TaskChanges properties

--- ToDo/Model/TaskChanges.swift
+++ ToDo/Model/TaskChanges.swift
@@ -1,8 +1,21 @@
-//
-//  TaskChanges.swift
-//  ToDo
-//
-//  Created by Mirelle Alessandre on 28/07/24.
-//
-
 import Foundation
+
+struct TaskChanges: Encodable {
+	let description: String
+	let dueDate: String
+	
+	init(description: String, dueDate: String) {
+		self.description = description
+		self.dueDate = dueDate
+	}
+	
+	init(description: String) {
+		self.description = description
+		dueDate = ""
+	}
+	
+	init(dueDate: String) {
+		description = ""
+		self.dueDate = dueDate
+	}
+}
