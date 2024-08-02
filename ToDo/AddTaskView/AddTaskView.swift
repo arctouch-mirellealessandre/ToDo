f98058086d1afed49be7ac7095bcdb25954c7f18
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-08-02 11:52:14 -0300

    Add LoadingView in AddTaskView

--- ToDo/AddTaskView/AddTaskView.swift
+++ ToDo/AddTaskView/AddTaskView.swift
@@ -5,8 +5,11 @@ import SwiftUI
 class AddTaskViewModel: ObservableObject {
 	var taskService: TaskService
 	
+	@Published var isAddingNewTask: Bool
+	
 	init(taskService: TaskService) {
 		self.taskService = taskService
+		self.isAddingNewTask = false
 	}
 	
 	func addNewTask(_ description: String, _ dueDate: String) {
@@ -17,6 +20,7 @@ class AddTaskViewModel: ObservableObject {
 				print("Error: couldn't add new task")
 			}
 		}
+		isAddingNewTask = false
 	}
 }
 
@@ -54,12 +58,17 @@ struct AddTaskView: View {
 				}
 			}
 			Button {
+				addTaskViewModel.isAddingNewTask = true
 				addTaskViewModel.addNewTask(description, dueDate)
 			} label: {
 				Text("Add Task")
 			}
 			.buttonStyle(.borderedProminent)
 			.offset(x: 0, y: -100)
+			
+			if addTaskViewModel.isAddingNewTask {
+				LoadingView(message: "Adding task")
+			}
 		}
 	}
 }
