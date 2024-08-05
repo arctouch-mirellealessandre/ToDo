77202993f506256fd556d1f9a94b65ddc6d35c8f
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-08-05 10:58:23 -0300

    Create DeleteTaskView file

---
+++ ToDo/DeleteTaskView/DeleteTaskView.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteTaskView.swift
+//  ToDo
+//
+//  Created by Mirelle Alessandre on 05/08/24.
+//
+
+import SwiftUI
+
+struct DeleteTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeleteTaskView()
+}
---
+++ ToDo/DeleteTaskView/DeleteTaskViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DeleteTaskViewModel.swift
+//  ToDo
+//
+//  Created by Mirelle Alessandre on 05/08/24.
+//
+
+import Foundation
